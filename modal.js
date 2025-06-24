9cccf3aa4192b70a75102fc38506882178f430b9
Ing. Stivenson Mussa <smring.designs@gmail.com>
2025-06-23 23:40:24 -0500

    Commits 5 Update

---
+++ modal.js
@@ -0,0 +1,37 @@
+/*Modal para servicios*/
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// BONUS: Cerrar al hacer clic fuera del modal (opcional)
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
+
+
+/*Modal para procesos*/ 
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
